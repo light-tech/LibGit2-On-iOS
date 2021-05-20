c6fb05d42fa218e3d9373870981570d1ca739c44
Lightech <lightech@outlook.com>
2021-05-20 14:41:40 +0700

    Build PCRE dependency

--- build-libgit2-framework.sh
+++ build-libgit2-framework.sh
@@ -1,17 +1,21 @@
 export REPO_ROOT=`pwd`
 export PATH=$PATH:$REPO_ROOT/tools/bin
+
+function build_libgit2() {
 git clone --recursive https://github.com/libgit2/libgit2.git
 cd libgit2
 # git submodule update --recursive
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install \
+cmake -DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install/libgit2 \
       -DBUILD_SHARED_LIBS=NO \
       -DBUILD_CLAR=NO \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_FLAGS="-target x86_64-apple-ios14.1-macabi" \
       ..
 
+#cd ..
 #cmake -L
+#cd build
 
 cmake --build . --target install
 
@@ -20,3 +24,29 @@ FRAMEWORKS_ARGS=()
 FRAMEWORKS_ARGS+=("-library" "install/lib/libgit2.a" "-headers" "install/include")
 xcodebuild -create-xcframework ${FRAMEWORKS_ARGS[@]} -output Clibgit2.xcframework
 tar -cJf Clibgit2.xcframework.tar.xz Clibgit2.xcframework
+}
+
+function build_pcre() {
+wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz # https://ftp.pcre.org/pub/pcre/pcre2-10.36.tar.gz
+tar xzf pcre-8.44.tar.gz
+cd pcre-8.44
+
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install/pcre \
+      -DBUILD_SHARED_LIBS=NO \
+      -DPCRE_BUILD_PCRECPP=NO \
+      -DPCRE_BUILD_PCREGREP=NO \
+      -DPCRE_BUILD_TESTS=NO \
+      -DPCRE_SUPPORT_LIBBZ2=NO \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_C_FLAGS="-target x86_64-apple-ios14.1-macabi" \
+      ..
+
+cd ..
+cmake -L
+cd build
+
+cmake --build . --target install
+}
+
+build_pcre maccatalyst
