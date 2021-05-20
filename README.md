0617e69923fb15262a508c80957b8a470ce0b2e2
Lightech <lightech@outlook.com>
2021-05-20 20:48:18 +0700

    Add the platform string as argument to build functions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 LibGit2 on iOS
 ==============
 
-Similar to our [LLVM-On-iOS project](https://github.com/light-tech/LLVM-On-iOS), this project provides scripts and YAML pipeline to build [LibGit2](github.com/libgit2/libgit2) and its dependencies (OpenSSL and LibSSH2) on Azure DevOps (a.k.a. VSTS).
+Similar to our [LLVM-On-iOS project](https://github.com/light-tech/LLVM-On-iOS) and thank to the experience from that project, this project provides scripts and YAML pipeline to build [LibGit2](github.com/libgit2/libgit2) and its dependencies (OpenSSL, PCRE and LibSSH2) on Azure DevOps (a.k.a. VSTS).
--- build-libgit2-framework.sh
+++ build-libgit2-framework.sh
@@ -1,58 +1,112 @@
 export REPO_ROOT=`pwd`
 export PATH=$PATH:$REPO_ROOT/tools/bin
 
+### Setup common environment variables to run CMake for a given platform
+### Usage:      setup_variables PLATFORM
+### where PLATFORM is the platform to build for and should be one of
+###    iphoneos
+###    iphonesimulator
+###    maccatalyst
+### (macos and M1 macos to be added in the future)
+###
+### After this function is executed, the variable $PLATFORM and $CMAKE_ARGS
+### providing basic/common CMake options will be set.
+###
+function setup_variables() {
+	PLATFORM=$1
+
+	CMAKE_ARGS=(-DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release)
+
+	case $PLATFORM in
+		"iphoneos")
+			SYSROOT=`xcodebuild -version -sdk iphoneos Path`
+			CMAKE_ARGS+=("-DCMAKE_OSX_SYSROOT=$SYSROOT");;
+		"iphonesimulator")
+			SYSROOT=`xcodebuild -version -sdk iphonesimulator Path`
+			CMAKE_ARGS+=("-DCMAKE_OSX_SYSROOT=$SYSROOT");;
+		"maccatalyst")
+			# No need to set up SYSROOT as CMake should find MacOS SDK automatically
+			echo "Set up variables for Mac Catalyst";;
+		*)
+			echo "Unsupported or missing platform!"
+			echo "Usage: setup_variables [iphoneos|iphonesimulator|maccatalyst]";;
+	esac
+}
+
+### Build libgit2 for a single platform (given as the first and only argument)
+### See @setup_variables for the list of available platform names
+###
 function build_libgit2() {
-git clone --recursive https://github.com/libgit2/libgit2.git
-cd libgit2
-# git submodule update --recursive
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install/libgit2 \
-      -DBUILD_SHARED_LIBS=NO \
-      -DBUILD_CLAR=NO \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_C_FLAGS="-target x86_64-apple-ios14.1-macabi" \
-      ..
+	setup_variables $1
 
-#cd ..
-#cmake -L
-#cd build
+	git clone --recursive https://github.com/libgit2/libgit2.git
+	cd libgit2
+	# git submodule update --recursive
 
-cmake --build . --target install
+	mkdir build && cd build
+	CMAKE_ARGS+=(-DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install/libgit2-$PLATFORM
+		-DBUILD_CLAR=NO)
 
-cd $REPO_ROOT
-FRAMEWORKS_ARGS=()
-FRAMEWORKS_ARGS+=("-library" "install/libgit2/lib/libgit2.a" "-headers" "install/libgit2/include")
-xcodebuild -create-xcframework ${FRAMEWORKS_ARGS[@]} -output Clibgit2.xcframework
-tar -cJf Clibgit2.xcframework.tar.xz Clibgit2.xcframework
+	case $PLATFORM in
+		"iphoneos"|"iphonesimulator")
+			cmake ${CMAKE_ARGS[@]} ..;;
+		"maccatalyst")
+			cmake ${CMAKE_ARGS[@]} -DCMAKE_C_FLAGS="-target x86_64-apple-ios14.1-macabi" ..;;
+	esac
+
+	#cd ..
+	#cmake -L
+	#cd build
+
+	cmake --build . --target install
 }
 
+function build_libgit2_xcframework() {
+	cd $REPO_ROOT
+	FRAMEWORKS_ARGS=()
+	FRAMEWORKS_ARGS+=("-library" "install/libgit2/lib/libgit2.a" "-headers" "install/libgit2/include")
+	xcodebuild -create-xcframework ${FRAMEWORKS_ARGS[@]} -output Clibgit2.xcframework
+	tar -cJf Clibgit2.xcframework.tar.xz Clibgit2.xcframework
+}
+
+### Build libpcre for a single platform
+### See @setup_variables for the list of available platform names
+###
 function build_pcre() {
-wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz # https://ftp.pcre.org/pub/pcre/pcre2-10.36.tar.gz
-tar xzf pcre-8.44.tar.gz
-cd pcre-8.44
+	setup_variables $1
 
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install/pcre \
-      -DBUILD_SHARED_LIBS=NO \
-      -DPCRE_BUILD_PCRECPP=NO \
-      -DPCRE_BUILD_PCREGREP=NO \
-      -DPCRE_BUILD_TESTS=NO \
-      -DPCRE_SUPPORT_LIBBZ2=NO \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_C_FLAGS="-target x86_64-apple-ios14.1-macabi" \
-      ..
+	wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz # https://ftp.pcre.org/pub/pcre/pcre2-10.36.tar.gz
+	tar xzf pcre-8.44.tar.gz
+	cd pcre-8.44
 
-cd ..
-cmake -L
-cd build
+	mkdir build && cd build
+	CMAKE_ARGS+=(-DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install/pcre-$PLATFORM \
+		-DPCRE_BUILD_PCRECPP=NO \
+		-DPCRE_BUILD_PCREGREP=NO \
+		-DPCRE_BUILD_TESTS=NO \
+		-DPCRE_SUPPORT_LIBBZ2=NO)
 
-cmake --build . --target install
+	case $PLATFORM in
+		"iphoneos"|"iphonesimulator")
+			cmake ${CMAKE_ARGS[@]} ..;;
+		"maccatalyst")
+			cmake ${CMAKE_ARGS[@]} -DCMAKE_C_FLAGS="-target x86_64-apple-ios14.1-macabi" ..;;
+	esac
 
-cd $REPO_ROOT
-FRAMEWORKS_ARGS=()
-FRAMEWORKS_ARGS+=("-library" "install/pcre/lib/libpcre.a" "-headers" "install/pcre/include")
-xcodebuild -create-xcframework ${FRAMEWORKS_ARGS[@]} -output LibPCRE.xcframework
-tar -cJf LibPCRE.xcframework.tar.xz LibPCRE.xcframework
+	cd ..
+	cmake -L
+	cd build
+
+	cmake --build . --target install
 }
 
+function build_pcre_framework() {
+	cd $REPO_ROOT
+	FRAMEWORKS_ARGS=()
+	FRAMEWORKS_ARGS+=("-library" "install/pcre/lib/libpcre.a" "-headers" "install/pcre/include")
+	xcodebuild -create-xcframework ${FRAMEWORKS_ARGS[@]} -output LibPCRE.xcframework
+	tar -cJf LibPCRE.xcframework.tar.xz LibPCRE.xcframework
+}
+
+build_libgit2 maccatalyst
 build_pcre maccatalyst
