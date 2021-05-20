f5e8163087262621716220a765c239a3dc228f75
Lightech <lightech@outlook.com>
2021-05-20 09:46:33 +0700

    Add some CMake configuration to build for maccatalyst and install locally in the repo

--- build-libgit2-framework.sh
+++ build-libgit2-framework.sh
@@ -4,5 +4,13 @@ git clone --recursive https://github.com/libgit2/libgit2.git
 cd libgit2
 # git submodule update --recursive
 mkdir build && cd build
-cmake ..
-cmake --build .
+cmake -DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install \
+      -DBUILD_SHARED_LIBS=NO \
+      -DBUILD_CLAR=NO \
+      -DCMAKE_C_FLAGS="-target x86_64-apple-ios14.1-macabi" \
+      -DCMAKE_CXX_FLAGS="-target x86_64-apple-ios14.1-macabi" \
+      ..
+
+cmake -L
+
+cmake --build . --target install
