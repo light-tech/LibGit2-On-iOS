36f5003ef952aaf22866d4487ba768fce5351a7a
Lightech <lightech@outlook.com>
2021-05-20 09:55:59 +0700

    Remove unused -DCMAKE_CXX_FLAGS

--- build-libgit2-framework.sh
+++ build-libgit2-framework.sh
@@ -7,10 +7,10 @@ mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install \
       -DBUILD_SHARED_LIBS=NO \
       -DBUILD_CLAR=NO \
+      -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_FLAGS="-target x86_64-apple-ios14.1-macabi" \
-      -DCMAKE_CXX_FLAGS="-target x86_64-apple-ios14.1-macabi" \
       ..
 
-cmake -L
+#cmake -L
 
 cmake --build . --target install
