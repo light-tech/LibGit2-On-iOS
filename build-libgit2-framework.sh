4a4e07e5d5dac17928007b1e4e4ecd37db468c11
Lightech <lightech@outlook.com>
2021-11-14 01:22:09 -0500

    Disable some commands output

--- build-libgit2-framework.sh
+++ build-libgit2-framework.sh
@@ -2,21 +2,21 @@ export REPO_ROOT=`pwd`
 export PATH=$PATH:$REPO_ROOT/tools/bin
 
 # There are limitations in `xcodebuild` command that disallow maccatalyst and maccatalyst-arm64
-# to be used simultaneously: One will get an error
+# to be used simultaneously: Doing that and we will get an error
 #
-#       Both ios-x86_64-maccatalyst and ios-arm64-maccatalyst represent two equivalent library definitions.
+#   Both ios-x86_64-maccatalyst and ios-arm64-maccatalyst represent two equivalent library definitions.
 #
-# in that case. `lipo` is probably needed in this case.
-# Likewise, `maccatalyst` and `macosx` cannot be used together. So probably one will needs multiple
-# xcframeworks for x86_64-based and ARM-based Mac development computer.
+# To provide binary for both, `lipo` is probably needed.
+# Likewise, `maccatalyst` and `macosx` cannot be used together. So unfortunately for now, one will
+# needs multiple xcframeworks for x86_64-based and ARM-based Mac development computer.
 
-# iphoneos maccatalyst maccatalyst-arm64 macosx macosx-arm64
-AVAILABLE_PLATFORMS=(iphonesimulator)
-
-AVAILABLE_FRAMEWORKS=(libpcre openssl libssh2 libgit2)
+# Note that iphonesimulator MUST be built first before other platform for otherwise, the lib built
+# somehow contains both x86_64 and arm64 binary leading to undefined symbols error!?
+# maccatalyst-arm64 macosx macosx-arm64
+AVAILABLE_PLATFORMS=(iphonesimulator iphoneos maccatalyst)
 
 # Download build tools
-test -d tools || wget https://github.com/light-tech/LLVM-On-iOS/releases/download/llvm12.0.0/tools.tar.xz
+test -d tools || wget -q https://github.com/light-tech/LLVM-On-iOS/releases/download/llvm12.0.0/tools.tar.xz
 tar xzf tools.tar.xz
 
 ### Setup common environment variables to run CMake for a given platform
@@ -91,8 +91,8 @@ function build_libgit2() {
 	# git submodule update --recursive
 
 	rm -rf libgit2-1.3.0
-	test -f libgit2-1.3.0.zip || wget https://github.com/libgit2/libgit2/archive/refs/tags/v1.3.0.zip
-	unzip v1.3.0.zip #tar xzf libgit2-1.3.0.tar.gz
+	test -f v1.3.0.zip || wget -q https://github.com/libgit2/libgit2/archive/refs/tags/v1.3.0.zip
+	unzip v1.3.0.zip >/dev/null #tar xzf libgit2-1.3.0.tar.gz
 	cd libgit2-1.3.0
 
 	rm -rf build && mkdir build && cd build
@@ -109,9 +109,9 @@ function build_libgit2() {
 
 	# Must add "" around ${CMAKE_ARGS[@]} since the array element can have space!
 	# See https://stackoverflow.com/questions/9084257/bash-array-with-spaces-in-elements
-	cmake "${CMAKE_ARGS[@]}" ..
+	cmake "${CMAKE_ARGS[@]}" .. >/dev/null 2>/dev/null
 
-	cmake --build . --target install >/dev/null
+	cmake --build . --target install >/dev/null 2>/dev/null
 }
 
 ### Build libpcre for a given platform
@@ -128,9 +128,9 @@ function build_libpcre() {
 		-DPCRE_BUILD_TESTS=NO \
 		-DPCRE_SUPPORT_LIBBZ2=NO)
 
-	cmake "${CMAKE_ARGS[@]}" ..
+	cmake "${CMAKE_ARGS[@]}" .. >/dev/null 2>/dev/null
 
-	cmake --build . --target install >/dev/null
+	cmake --build . --target install >/dev/null 2>/dev/null
 }
 
 ### Build openssl for a given platform
@@ -139,7 +139,7 @@ function build_openssl() {
 
 	# It is better to remove and redownload the source since building make the source code directory dirty!
 	rm -rf openssl-3.0.0
-	test -f openssl-3.0.0.tar.gz || wget https://www.openssl.org/source/openssl-3.0.0.tar.gz
+	test -f openssl-3.0.0.tar.gz || wget -q https://www.openssl.org/source/openssl-3.0.0.tar.gz
 	tar xzf openssl-3.0.0.tar.gz
 	cd openssl-3.0.0
 
@@ -167,10 +167,10 @@ function build_openssl() {
 	# See https://wiki.openssl.org/index.php/Compilation_and_Installation
 	./Configure --prefix=$REPO_ROOT/install/$PLATFORM \
 		--openssldir=$REPO_ROOT/install/$PLATFORM \
-		$TARGET_OS no-shared no-dso no-hw no-engine
+		$TARGET_OS no-shared no-dso no-hw no-engine >/dev/null 2>/dev/null
 
-	make >/dev/null
-	make install_sw install_ssldirs >/dev/null
+	make >/dev/null 2>/dev/null
+	make install_sw install_ssldirs >/dev/null 2>/dev/null
 }
 
 ### Build libssh2 for a given platform (assume openssl was built)
@@ -178,7 +178,7 @@ function build_libssh2() {
 	setup_variables $1
 
 	rm -rf libssh2-1.10.0
-	test -f libssh2-1.10.0.tar.gz || wget https://github.com/libssh2/libssh2/releases/download/libssh2-1.10.0/libssh2-1.10.0.tar.gz
+	test -f libssh2-1.10.0.tar.gz || wget -q https://github.com/libssh2/libssh2/releases/download/libssh2-1.10.0/libssh2-1.10.0.tar.gz
 	tar xzf libssh2-1.10.0.tar.gz
 	cd libssh2-1.10.0
 
@@ -189,9 +189,9 @@ function build_libssh2() {
 		-DBUILD_EXAMPLES=OFF \
 		-DBUILD_TESTING=OFF)
 
-	cmake "${CMAKE_ARGS[@]}" ..
+	cmake "${CMAKE_ARGS[@]}" .. >/dev/null 2>/dev/null
 
-	cmake --build . --target install >/dev/null
+	cmake --build . --target install >/dev/null 2>/dev/null
 }
 
 ### Copy SwiftGit2's module.modulemap to libgit2.xcframework/*/Headers
